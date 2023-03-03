c35382f944b8bc4372b4e9ffcafa2da3f2adbd1c
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-03-04 03:03:52 +0530

    html, CSS, API part

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
+ 
 const app = express();
-
-app.listen(3002, function(){
-    console.log("Server is running");
+app.use(bodyParser.urlencoded({ extended: true}));
+ 
+const port = 3002;
+ 
+// app.use(express.static('public'));
+app.use(express.static(__dirname));
+ 
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html")
 })
+
+app.post("/", function(req,res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+})
+ 
+app.listen(port, function() {
+     console.log(`Example app listening at http://localhost:${port}`);
+});
